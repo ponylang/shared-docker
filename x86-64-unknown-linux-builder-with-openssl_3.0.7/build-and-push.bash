ccae04b958c89fd4c61d03cd2134f7fafdc0d870
Sean T Allen <sean@seantallen.com>
2022-12-31 16:03:15 -0500

    Add OpenSSL 3.0.7 based builder (#44)

---
+++ x86-64-unknown-linux-builder-with-openssl_3.0.7/build-and-push.bash
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to DockerHub when you run this ***
+#
+
+DOCKERFILE_DIR="$(dirname "$0")"
+IMAGE="ponylang/shared-docker-ci-x86-64-unknown-linux-builder-with-openssl_3.0.7"
+
+# built from x86-64-unknown-linux-builder release tag
+FROM_TAG=release
+TAG_AS=release
+docker build --pull --build-arg FROM_TAG="${FROM_TAG}" \
+  -t "${IMAGE}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${IMAGE}:${TAG_AS}"
+
+# built from x86-64-unknown-linux-builder latest tag
+FROM_TAG=latest
+TAG_AS=latest
+docker build --pull --build-arg FROM_TAG="${FROM_TAG}" \
+  -t "${IMAGE}:${TAG_AS}" "${DOCKERFILE_DIR}"
+docker push "${IMAGE}:${TAG_AS}"
