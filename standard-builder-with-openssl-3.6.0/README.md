6c12cc437e2ee1868c2a5c6b55449721bfe918c1
Sean T. Allen <sean@seantallen.com>
2025-10-18 19:21:29 -0400

    Add OpenSSL 3.6.0 builder

---
+++ standard-builder-with-openssl-3.6.0/README.md
@@ -0,0 +1,3 @@
+# standard-builder-with-openssl-3.6.0
+
+The standard-builder with OpenSSL 3.6.0 installed as well. Rebuilt daily.
---
+++ standard-builder-with-openssl-3.6.0/build-and-push.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to GitHub Container Registry when you run
+#     this ***
+#
+
+DOCKERFILE_DIR="$(dirname "$0")"
+BUILDER="standard-builder-with-openssl-3.6.0-$(date +%s)"
+NAME="ghcr.io/ponylang/shared-docker-ci-standard-builder-with-openssl-3.6.0"
+
+echo "Building nightly image from standard-builder nightly tag"
+docker buildx create --use --name "${BUILDER}"
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push --build-arg \
+  FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"
+docker buildx rm "${BUILDER}"
+
+echo "Building release image from standard-builder release tag"
+docker buildx create --use --name "${BUILDER}"
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push --build-arg \
+  FROM_TAG="release" -t "${NAME}:release" "${DOCKERFILE_DIR}"
+docker buildx rm "${BUILDER}"
