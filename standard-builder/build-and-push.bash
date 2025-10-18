aab2af41035b9366c03336572b2814c6500aa4d7
Sean T. Allen <sean@seantallen.com>
2025-10-18 14:14:57 -0400

    Another attempt to fix broken manifest for standard builder

--- standard-builder/build-and-push.bash
+++ standard-builder/build-and-push.bash
@@ -12,16 +12,16 @@ DOCKERFILE_DIR="$(dirname "$0")"
 BUILDER="standard-builder-$(date +%s)"
 NAME="ghcr.io/ponylang/shared-docker-ci-standard-builder"
 
-docker buildx create --use --name "${BUILDER}"
-
 echo "Building nightly image from ponyc nightly tag"
+docker buildx create --use --name "${BUILDER}"
 docker buildx build --provenance false --sbom false \
   --platform linux/arm64,linux/amd64 --pull --push --build-arg \
   FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"
+docker buildx rm "${BUILDER}"
 
 echo "Building release image from ponyc release tag"
+docker buildx create --use --name "${BUILDER}"
 docker buildx build --provenance false --sbom false \
   --platform linux/arm64,linux/amd64 --pull --push --build-arg \
   FROM_TAG="release" -t "${NAME}:release" "${DOCKERFILE_DIR}"
-
 docker buildx rm "${BUILDER}"
