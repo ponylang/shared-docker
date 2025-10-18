3bf19475d9b4cd7eb7a15e6df95507bdb2689d6a
Sean T. Allen <sean@seantallen.com>
2025-10-18 10:48:01 -0400

    Fix syntax error in standard builder build and push script
    Apparently, buildx is really picky about spaces.

--- standard-builder/build-and-push.bash
+++ standard-builder/build-and-push.bash
@@ -15,13 +15,13 @@ NAME="ghcr.io/ponylang/shared-docker-ci-standard-builder"
 docker buildx create --use --name "${BUILDER}"
 
 echo "Building release image from ponyc release tag"
-docker buildx build  --provenance false --sbom false \
-  --platform linux/arm64, linux/amd64 --pull --push --build-arg \
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push --build-arg \
   FROM_TAG="release" -t "${NAME}:release" "${DOCKERFILE_DIR}"
 
 echo "Building nightly image from ponyc nightly tag"
-docker buildx build  --provenance false --sbom false \
-  --platform linux/arm64, linux/amd64 --pull --push --build-arg \
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push --build-arg \
   FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"
 
 docker buildx rm "${BUILDER}"
