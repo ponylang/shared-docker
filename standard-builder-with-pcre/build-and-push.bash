64ca01acf7e1a2445a9f83fdf6ec7eee2d09f93f
Sean T. Allen <sean@seantallen.com>
2025-10-20 12:05:31 -0400

    Make "with pcre" image multiplatform
    This also renames it and shifts from "latest" to "nightly" as the tag.

---
+++ standard-builder-with-pcre/build-and-push.bash
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+#
+# *** You should already be logged in to GitHub Container Registry when you run
+#     this ***
+#
+
+DOCKERFILE_DIR="$(dirname "$0")"
+NAME="ghcr.io/ponylang/shared-docker-ci-standard-builder-with-pcre"
+BUILDER="standard-builder-with-pcre-$(date +%s)"
+
+echo "Building nightly image from standard-builder nightly tag"
+docker buildx create --use --name "${BUILDER}"
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push \
+  FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"docker buildx rm "${BUILDER}"
+
+echo "Building release image from standard-builder release tag"
+docker buildx create --use --name "${BUILDER}"
+docker buildx build --provenance false --sbom false \
+  --platform linux/arm64,linux/amd64 --pull --push \
+  FROM_TAG="release" -t "${NAME}:release" "${DOCKERFILE_DIR}"
+docker buildx rm "${BUILDER}"
--- x86-64-unknown-linux-builder-with-pcre/README.md
+++
@@ -1,3 +0,0 @@
-# x86-64-unknown-linux-builder-with-pcre
-
-The x86-64-unknown-linux-builder with an PCRE implementation installed as well. Rebuilt daily.
