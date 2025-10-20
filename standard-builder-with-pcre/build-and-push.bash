c709151c367aa83bbefbd3f4961ad07067a2f66e
Sean T. Allen <sean@seantallen.com>
2025-10-20 12:16:28 -0400

    Fix typo in build script

--- standard-builder-with-pcre/build-and-push.bash
+++ standard-builder-with-pcre/build-and-push.bash
@@ -16,7 +16,8 @@ echo "Building nightly image from standard-builder nightly tag"
 docker buildx create --use --name "${BUILDER}"
 docker buildx build --provenance false --sbom false \
   --platform linux/arm64,linux/amd64 --pull --push \
-  FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"docker buildx rm "${BUILDER}"
+  FROM_TAG="nightly" -t "${NAME}:nightly" "${DOCKERFILE_DIR}"
+docker buildx rm "${BUILDER}"
 
 echo "Building release image from standard-builder release tag"
 docker buildx create --use --name "${BUILDER}"
